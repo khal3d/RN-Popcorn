344de7b220adc5cc017ffea67bb39080507304bc
Khaled Attia <khaledattia@gmail.com>
2019-12-14 11:30:59 +0200

    Movied the App.js under the src directory

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import MoviesScreen from './src/components/screens/MoviesScreen';
-import BookmarkScreen from './src/components/screens/BookmarkScreen';
+import MoviesScreen from './components/screens/MoviesScreen';
+import BookmarkScreen from './components/screens/BookmarkScreen';
 
 export default class App extends Component {
   render() {
